2de31996dc783d548504cb9db8775b97bdb88298
arif <arif.mujawar@medibox.in>
2016-08-11 15:17:04 +0530

    10th commit animataion added

--- public/js/controllers/contactCtrl.js
+++ public/js/controllers/contactCtrl.js
@@ -1,7 +1,7 @@
 /**
  * Created by arif on 31/7/16.
  */
-angular.module('ProfileCtrl', []).controller('contactController', function($scope,$http) {
+angular.module('contactCtrl', []).controller('contactController', function($scope,$http) {
     console.log('initailazing contact');
 
     $scope.tagline = 'The square root of life is pi!';
