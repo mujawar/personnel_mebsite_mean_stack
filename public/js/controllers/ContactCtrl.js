59c5313feaa7bdf0212cfd9d6ff4871060445d77
arif <arif.mujawar@medibox.in>
2017-03-05 14:07:34 +0530

    reseme changed again

--- public/js/controllers/ContactCtrl.js
+++ public/js/controllers/ContactCtrl.js
@@ -1,28 +1,28 @@
 /**
  * Created by arif on 31/7/16.
  */
-angular.module('ContactCtrl', []).controller('ContactController', function($scope,$http) {
+angular.module('ContactCtrl', []).controller('ContactController', function ($scope, $http) {
     console.log('initailazing contact');
 
     $scope.tagline = 'The square root of life is pi!';
 
-    $scope.submitData = function(){
+    $scope.submitData = function () {
         console.log('calling submitData');
-        console.log('data' +JSON.stringify($scope.contact));
-        $http.post('/personnelwebsite',$scope.contact).success(function(result){
-            console.log('result' +JSON.stringify(result));
-            $scope.successMessage="Thanku for contacting me i will get back to you as earliest!"
+        console.log('data' + JSON.stringify($scope.contact));
+        $http.post('/personnelwebsite', $scope.contact).success(function (result) {
+            console.log('result' + JSON.stringify(result));
+            $scope.successMessage = "Thanku for contacting me i will get back to you as earliest!"
 
             var mailOptions = {
                 to: 'arifmr91@gmail.com',
                 subject: 'Email from my personnel website',
                 data: $scope.contact
             };
-            console.log('mailOptions' +JSON.stringify(mailOptions));
-           /* $http.post('/sendFeedbackMail',mailOptions).success(function(result) {
-                console.log('result' + JSON.stringify(result));
-            })
-*/
+            console.log('mailOptions' + JSON.stringify(mailOptions));
+            /* $http.post('/sendFeedbackMail',mailOptions).success(function(result) {
+             console.log('result' + JSON.stringify(result));
+             })
+             */
             $scope.contact = ''
 
 
